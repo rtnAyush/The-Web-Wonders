40f866152629b5f524b008b23d78c32fee7121b2
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2023-04-06 19:52:22 +0530

    Hackthon ends

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# echo "# The-Web-Wonders" >> README.md
-# git init
-
-
-git add .
-git commit -m "Add hostel leave"
-# git branch -M main
-# git remote add origin https://github.com/Ayush211107/The-Web-Wonders.git
-git push origin main
